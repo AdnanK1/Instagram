efbb5ab3ff6734be5208afee09d876575f2d247f
AdnanK1 <adnangitonga@gmail.com>
2022-06-09 14:49:31 +0300

    Base/forms:added postform and profileform

--- base/forms.py
+++ base/forms.py
@@ -2,9 +2,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from django import forms
-from .models import Image
+from .models import Image,Profile
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','hashtag','caption']
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['photo', 'Bio']
