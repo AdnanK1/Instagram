803c880b6b26368d10ad6cde0663d18102112d8c
AdnanK1 <adnangitonga@gmail.com>
2022-06-09 14:07:12 +0300

    Base/Templates/auth: added a navbar

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('login/',views.loginPage,name='login'),
-    path('register/',views.registerPage,name='register'),
+    path('',views.loginPage,name='login'),
+    path('register',views.registerPage,name='register'),
+    path('logout',views.logoutUser,name='logout'),
+
+    path('home',views.home,name='home'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def home(request):
@@ -7,6 +9,14 @@ def home(request):
     return render(request, 'home.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
     context = {}
     return render(request,'auth/login.html', context)
 
@@ -15,7 +25,15 @@ def registerPage(request):
     if request.method == 'POST':
         form=CreateUserForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
             form.save()
+            login(request,user)
             return redirect('home')
     context = {'form':form}
     return render(request,'auth/register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
