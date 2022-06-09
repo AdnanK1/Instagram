9710bfee9df6c1be15db32145a939f6c2921a595
AdnanK1 <adnangitonga@gmail.com>
2022-06-09 10:32:24 +0300

    Base/Models:renamed the post class to image class

--- base/models.py
+++ base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Post(models.Model):
+class Image(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     liked = models.ManyToManyField(User,blank=True,default=None, related_name='blog_posts')
