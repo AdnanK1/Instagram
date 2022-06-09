9560701de2cc57c4a44fe70a95e8f7f4019a13c7
AdnanK1 <adnangitonga@gmail.com>
2022-06-09 09:32:52 +0300

    Base:Displaying home page in the browser

--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'home.html', context)
